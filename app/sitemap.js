831eff308158929eda1d640bb70c41d4777214f5
parasmottan <parasmottan9316@gmail.com>
2025-09-24 20:37:17 +0530

    veloura

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,19 +1,16 @@
-// pages/sitemap.xml.js
-export default function handler(req, res) {
-  res.setHeader("Content-Type", "text/xml");
-  res.write(`<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>https://itsveloura.shop/</loc>
-    <lastmod>2025-09-24</lastmod>
-    <priority>1.0</priority>
-  </url>
-  <url>
-    <loc>https://itsveloura.shop/about</loc>
-    <lastmod>2025-09-24</lastmod>
-    <priority>0.8</priority>
-  </url>
-</urlset>
-`);
-  res.end();
+export default function sitemap() {
+  return [
+    {
+      url: "https://itsveloura.shop",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: "https://itsveloura.shop/about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ];
 }
